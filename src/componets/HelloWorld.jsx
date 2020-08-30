46ee69e3810c9bce71d363ce8386af9a08cdac82
Circuandante <circuandante@gmail.com>
2020-08-29 21:00:36 -0500

    eslint

---
+++ src/componets/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const Hello = () => (
+  <p>hola mundo</p>
+);
+
+export default Hello;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Hello from './componets/HelloWorld'
+
+// sass
+// import './assets/styles/App.scss'
+
+ReactDom.render(
+  <Hello />,
+  document.getElementById('app')
+);
+
